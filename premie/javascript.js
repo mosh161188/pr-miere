69107928f754519ef1528e5a585bb74c94000223
Angelo Walczak <angelowalczak@Angelos-MacBook-Pro.local>
2013-04-18 18:08:23 +0200

    Layout

--- premie/javascript.js
+++ premie/javascript.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
+
+	var handler = $('#tiles li');
+	var options = {
+        autoResize: true, // This will auto-update the layout when the browser window is resized.
+        container: $('#feed'), // Optional, used for some extra CSS styling
+        offset: 2, // Optional, the distance between grid items
+        itemWidth: 210 // Optional, the width of a grid item
+      };
+      // Call the layout function.
+      handler.wookmark(options);
+      handler.wookmark();
+      jsonFlickrApi();
  
- $('.navbarprofile').mouseover(function() {
-  showProfileDropdown();
- });
+	 $('.navbarprofile').mouseover(function() {
+	  showProfileDropdown();
+	 });
  
  	$("body").click(function(){
 	 	hideProfileDropdown();
@@ -24,4 +36,32 @@ function showProfileDropdown(){
 function hideProfileDropdown(){
 	$('.navbarprofile').css({'height':'38px'});
 	$('.dropdownbox').css({'visibility':'hidden'});
+}
+
+function jsonFlickrApi(rsp) {
+ if (rsp.stat != "ok"){
+  return;
+ }
+ 
+ var s = "";
+ var i = Math.random();
+ i = i * 100;
+ i = Math.ceil(i);
+ 
+ photo = rsp.photos.photo[ i ];
+ 
+ t_url = "http://farm" + photo.farm +
+ ".static.flickr.com/" + photo.server + "/" +
+ photo.id + "_" + photo.secret + "_" + "m.jpg";
+ 
+ p_url = "http://www.flickr.com/photos/" +
+ photo.owner + "/" + photo.id;
+ 
+ s =  '<img alt="'+ photo.title + '"src="' + t_url + '"/>'  ;
+ 
+ 
+ document.writeln(s);
+ //this tells the JavaScript to write
+ //everything in variable "s" onto the page
+ 
 }
